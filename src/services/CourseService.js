fbe6d9be2a7439631fa03b059506119711df6288
Jose Annunziato <jannunzi@gmail.com>
2019-05-23 22:12:19 -0400

    latest;

---
+++ src/services/CourseService.js
@@ -0,0 +1,30 @@
+import courses from './courses.json'
+
+class CourseService {
+
+    static myInstance = null;
+
+    static getInstance() {
+        if (CourseService.myInstance == null) {
+            CourseService.myInstance =
+                new CourseService();
+        }
+        return this.myInstance;
+    }
+
+    createCourse = course => {
+        courses.push(course)
+    }
+    findAllCourses = () => {
+        return courses
+    }
+    findCourseById = courseId => {
+        return courses.find(course => course.id == courseId)
+    }
+    updateCourse = (courseId, course) => {}
+    deleteCourse = courseId => {
+        courses = courses.filter(course => course.id !== courseId)
+    }
+}
+
+export default CourseService
---
+++ src/services/ModuleService.js
@@ -0,0 +1,31 @@
+import modules from './modules.json'
+
+export default class ModuleService {
+
+    static myInstance = null;
+
+    static getInstance() {
+        if (ModuleService.myInstance == null) {
+            ModuleService.myInstance =
+                new ModuleService();
+        }
+        return this.myInstance;
+    }
+
+    createModule = (courseId, module) => {
+        module['courseId'] = courseId
+        modules.push(module)
+    }
+    findAllModules = () =>
+        modules
+    findAllModuleForCourseById = courseId =>
+        modules.filter(module => module.courseId == courseId)
+    updateModuleForCourseId  = (courseId, newModule) => {
+        modules = modules.map(
+            module => module.courseId == courseId ?
+                newModule : module)
+    }
+    deleteModule = moduleId => {
+        modules = modules.filter(module => module.id !== moduleId)
+    }
+}
